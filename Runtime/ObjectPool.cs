6591ddbc6ac1d80f4e9e2fb3423c9ee40b28d5c5
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-22 05:50:33 +0200

    ⠀⠀⠀⠀⠀

---
+++ Runtime/ObjectPool.cs
@@ -0,0 +1,46 @@
+﻿namespace UnityEssentials
+{
+    public class ObjectPool<T> where T : struct
+    {
+        private T[] _pool;
+        private int _freeIndex;
+        private int _capacity;
+
+        public ObjectPool(int initialCapacity, System.Func<int, T> createItem)
+        {
+            _capacity = initialCapacity;
+            _pool = new T[_capacity];
+            _freeIndex = 0;
+
+            for (int i = 0; i < _capacity; i++)
+                _pool[i] = createItem(i);
+        }
+
+        public T Get()
+        {
+            if (_freeIndex >= _capacity)
+                ExpandPool(_capacity * 2);
+
+            T item = _pool[_freeIndex];
+            _freeIndex++;
+            return item;
+        }
+
+        public void Release(ref T item, int index)
+        {
+            if (index >= 0 && index < _capacity)
+            {
+                _pool[index] = item;
+                _freeIndex--;
+            }
+        }
+
+        private void ExpandPool(int newCapacity)
+        {
+            T[] newPool = new T[newCapacity];
+            System.Array.Copy(_pool, newPool, _capacity);
+            _pool = newPool;
+            _capacity = newCapacity;
+        }
+    }
+}
